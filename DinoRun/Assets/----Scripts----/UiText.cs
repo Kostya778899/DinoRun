faa215878e546ce95f20cfc8a6e15e654f5a30ba
Kostya778899 <89563229+Kostya778899@users.noreply.github.com>
2022-03-03 23:07:43 +0500

    Небольшие правки.

--- DinoRun/Assets/----Scripts----/UiText.cs
+++ DinoRun/Assets/----Scripts----/UiText.cs
@@ -7,15 +7,16 @@ using TMPro;
 [RequireComponent(typeof(TMP_Text))]
 public class UiText : MonoBehaviour
 {
+    [SerializeField] private string _text = "Value: {Value}.";
     [SerializeField] private string _valueField = "{Value}";
 
-    private TMP_Text _text;
+    private TMP_Text _uiText;
 
 
-    public void SetText(string value) => _text.SetText(value);
+    public void SetText(string value) => _uiText.SetText(_text = value);
 
-    public void SetValueField(string value) => SetText(_text.text.Replace(_valueField, value));
+    public void SetValueField(string value) => SetText(_uiText.text.Replace(_text, value));
     public void SetValueField(int value) => SetValueField(value.ToString());
 
-    private void Awake() => _text = GetComponent<TMP_Text>();
+    private void Awake() => _uiText = GetComponent<TMP_Text>();
 }
